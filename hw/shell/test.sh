44a8f7a24cd63bcf5bc8b23ebba7d56f5517e2c6
Li Cao <irvingcl@google.com>
2019-11-27 08:43:15 +0800

    [hw] add a test case

--- hw/shell/test.sh
+++ hw/shell/test.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-echo "echo 'abc def ghi jkl mno pqr stu vwxyz' | wc | sed 's/^ *//g'" | ./a.out
+# Color
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+# test 1
+echo "echo 'abc abc def jkl mno pqr stu vwxyz' | wc -w" | ./a.out > tmp_check
+
+check_content=$(cat tmp_check)
+if [ $check_content -eq 8 ]
+then
+    echo "Case#1: ${GREEN}Passed"
+else
+    echo "Case#1: ${RED}Failed"
+fi
+
+rm tmp_check
